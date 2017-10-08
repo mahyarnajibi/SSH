7308a2f5503ab7e3dd41352eddf4d36ade6075f3
Mahyar Najibi <mahyarnajibi@gmail.com>
2017-10-08 17:30:16 -0400

    Adding the ROI data layer package

---
+++ lib/roi_data_layer/layer.py
@@ -0,0 +1,103 @@
+# ------------------------------------------------------------------------------------------------
+# This file is a modified version of https://github.com/rbgirshick/py-faster-rcnn by Ross Girshick
+# Modified by Mahyar Najibi
+# ------------------------------------------------------------------------------------------------
+import caffe
+import numpy as np
+import yaml
+
+from utils.get_config import cfg
+from roi_data_layer.minibatch import get_minibatch
+
+
+class RoIDataLayer(caffe.Layer):
+
+    def _shuffle_roidb_inds(self, gpu_id=0):
+        self.gpu_id = gpu_id
+        """Randomly permute the training roidb."""
+        if cfg.TRAIN.ASPECT_GROUPING:
+            widths = np.array([r['width'] for r in self._roidb])
+            heights = np.array([r['height'] for r in self._roidb])
+            horz = (widths >= heights)
+            vert = np.logical_not(horz)
+            horz_inds = np.where(horz)[0]
+            vert_inds = np.where(vert)[0]
+            inds = np.hstack((
+                np.random.permutation(horz_inds),
+                np.random.permutation(vert_inds)))
+            inds = np.reshape(inds, (-1, 2))
+            np.random.seed(gpu_id)
+            row_perm = np.random.permutation(np.arange(inds.shape[0]))
+            inds = np.reshape(inds[row_perm, :], (-1,))
+            self._perm = inds
+        else:
+            self._perm = np.random.permutation(np.arange(len(self._roidb)))
+        self._cur = 0
+
+    def _get_next_minibatch_inds(self):
+        """Return the roidb indices for the next minibatch."""
+        if self._cur + cfg.TRAIN.IMS_PER_BATCH >= len(self._roidb):
+            self._shuffle_roidb_inds(self.gpu_id)
+
+        db_inds = self._perm[self._cur:self._cur + cfg.TRAIN.IMS_PER_BATCH]
+        self._cur += cfg.TRAIN.IMS_PER_BATCH
+        return db_inds
+
+    def _get_next_minibatch(self):
+        """Return the blobs to be used for the next minibatch.
+        """
+        db_inds = self._get_next_minibatch_inds()
+        minibatch_db = [self._roidb[i] for i in db_inds]
+        return get_minibatch(minibatch_db)
+
+    def set_roidb(self, roidb, gpu_id=0):
+        """Set the roidb to be used by this layer during training."""
+        self._roidb = roidb
+        self._shuffle_roidb_inds(gpu_id)
+
+    def setup(self, bottom, top):
+        """Setup the RoIDataLayer."""
+
+        # parse the layer parameter string, which must be valid YAML
+        layer_params = yaml.load(self.param_str)
+
+        self._num_classes = layer_params['num_classes']
+
+        self._name_to_top_map = {}
+
+        # data blob: holds a batch of N images, each with 3 channels
+        idx = 0
+        top[idx].reshape(cfg.TRAIN.IMS_PER_BATCH, 3,
+            max(cfg.TRAIN.SCALES), cfg.TRAIN.MAX_SIZE)
+        self._name_to_top_map['data'] = idx
+        idx += 1
+
+        top[idx].reshape(1, 3)
+        self._name_to_top_map['im_info'] = idx
+        idx += 1
+
+        top[idx].reshape(1, 4)
+        self._name_to_top_map['gt_boxes'] = idx
+        idx += 1
+
+        assert len(top) == len(self._name_to_top_map)
+
+    def forward(self, bottom, top):
+        """Get blobs and copy them into this layer's top blob vector."""
+        blobs = self._get_next_minibatch()
+
+        for blob_name, blob in blobs.iteritems():
+            top_ind = self._name_to_top_map[blob_name]
+            # Reshape net's input blobs
+            top[top_ind].reshape(*(blob.shape))
+            # Copy data into net's input blobs
+            top[top_ind].data[...] = blob.astype(np.float32, copy=False)
+
+    def backward(self, top, propagate_down, bottom):
+        """This layer does not propagate gradients."""
+        pass
+
+    def reshape(self, bottom, top):
+        """Reshaping happens during the call to forward."""
+        pass
+
---
+++ lib/roi_data_layer/minibatch.py
@@ -0,0 +1,64 @@
+# ------------------------------------------------------------------------------------------------
+# This file is a modified version of https://github.com/rbgirshick/py-faster-rcnn by Ross Girshick
+# Modified by Mahyar Najibi
+# ------------------------------------------------------------------------------------------------
+import cv2
+import numpy as np
+import numpy.random as npr
+from utils.get_config import cfg
+from utils.blob import prep_im_for_blob, im_list_to_blob
+
+
+def get_minibatch(roidb):
+    """Return the mini-batch for training"""
+    num_images = len(roidb)
+    random_scale_inds = npr.randint(0, high=len(cfg.TRAIN.SCALES),
+                                    size=num_images)
+    assert(cfg.TRAIN.BATCH_SIZE % num_images == 0), \
+        'num_images ({}) must divide BATCH_SIZE ({})'. \
+        format(num_images, cfg.TRAIN.BATCH_SIZE)
+
+    im_blob, im_scales = _get_image_blob(roidb, random_scale_inds)
+
+    blobs = {'data': im_blob}
+
+    assert len(im_scales) == 1, "Single batch only"
+    assert len(roidb) == 1, "Single batch only"
+    # gt boxes: (x1, y1, x2, y2, cls)
+    gt_inds = np.where(roidb[0]['gt_classes'] != 0)[0]
+    gt_boxes = np.empty((len(gt_inds), 5), dtype=np.float32)
+    gt_boxes[:, 0:4] = roidb[0]['boxes'][gt_inds, :] * im_scales[0]
+    gt_boxes[:, 4] = roidb[0]['gt_classes'][gt_inds]
+    blobs['gt_boxes'] = gt_boxes
+    blobs['im_info'] = np.array(
+        [[im_blob.shape[2], im_blob.shape[3], im_scales[0]]],
+        dtype=np.float32)
+
+    return blobs
+
+
+def _get_image_blob(roidb, scale_inds):
+    num_images = len(roidb)
+    processed_ims = []
+    im_scales = []
+    for i in xrange(num_images):
+        im = cv2.imread(roidb[i]['image'])
+
+        if roidb[i]['flipped']:
+            im = im[:, ::-1, :]
+
+        target_size = cfg.TRAIN.SCALES[scale_inds[i]]
+        im, im_scale = prep_im_for_blob(im, cfg.PIXEL_MEANS, target_size,
+                                        cfg.TRAIN.MAX_SIZE)
+        im_scales.append(im_scale)
+        processed_ims.append(im)
+
+    blob = im_list_to_blob(processed_ims)
+    return blob, im_scales
+
+
+def _project_im_rois(im_rois, im_scale_factor):
+    """Project image RoIs into the rescaled training image."""
+    rois = im_rois * im_scale_factor
+    return rois
+
