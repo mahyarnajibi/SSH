ac437389623b8167118069ac8023562f9a5f01fb
Mahyar Najibi <mahyarnajibi@gmail.com>
2017-10-09 14:37:29 -0400

    Adding scripts for downloading models

---
+++ scripts/download_imgnet_model.sh
@@ -0,0 +1,32 @@
+#-----------------------------------------
+# SSH: Single Stage Headless Face Detector
+# Download script
+#-----------------------------------------
+#!/bin/bash
+url="www.umiacs.umd.edu/~najibi/download.php"
+method_name="vgg"
+file_name="imagenet_models.tar.gz"
+cur_dir=${PWD##*/}
+target_dir="./data/"
+
+if [ $cur_dir = "scripts" ]; then
+   target_dir="../data/"
+fi
+
+if [ ! -f ${file_name} ]; then
+   echo "Downloading ${file_name}..."
+   wget "${url}?name=${method_name}" -O "${target_dir}${file_name}"
+   echo "Done!"
+else
+   echo "File already exists!"
+fi
+
+echo "Unzipping the file..."
+tar -xvzf "${target_dir}${file_name}" -C ${target_dir}
+
+echo "Cleaning up..."
+rm "${target_dir}${file_name}"
+echo "All done!"
+
+
+
