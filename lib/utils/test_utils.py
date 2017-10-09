5ad6f71300508018cc80792676f56d65165ced18
Mahyar Najibi <mahyarnajibi@gmail.com>
2017-10-09 14:05:41 -0400

    Adding a function for visualizing the detections

--- lib/utils/test_utils.py
+++ lib/utils/test_utils.py
@@ -3,6 +3,9 @@
 # Utilities used in SSH test modules
 # Written by Mahyar Najibi
 # --------------------------------------------------------------------------------------------------
+import os
+os.environ['QT_QPA_PLATFORM']='offscreen'
+import matplotlib.pyplot as plt
 from utils.get_config import cfg
 import numpy as np
 import cv2
@@ -48,4 +51,59 @@ def _get_image_blob(im,im_scales):
             blobs.append({'data':im_list_to_blob([cv2.resize(im_copy, None, None, fx=scale, fy=scale,
                             interpolation=cv2.INTER_LINEAR)])})
     return blobs
+
+
+def visusalize_detections(im, bboxes, plt_name='output', ext='.png', visualization_folder=None, thresh=0.5):
+    """
+    A function to visualize the detections
+    :param im: The image
+    :param bboxes: The bounding box detections
+    :param plt_name: The name of the plot
+    :param ext: The save extension (if visualization_folder is not None)
+    :param visualization_folder: The folder to save the results
+    :param thresh: The detections with a score less than thresh are not visualized
+    """
+    inds = np.where(bboxes[:, -1] >= thresh)[0]
+    bboxes = bboxes[inds]
+    fig, ax = plt.subplots(figsize=(12, 12))
+
+
+    if im.shape[0] == 3:
+        im_cp = im.copy()
+        im_cp = im_cp.transpose((1, 2, 0))
+        if im.min() < 0:
+            pixel_means = cfg.PIXEL_MEANS
+            im_cp = im_cp + pixel_means
+
+        im = im_cp.astype(dtype=np.uint8)
+
+    im = im[:, :, (2, 1, 0)]
+
+    ax.imshow(im, aspect='equal')
+    if bboxes.shape[0] != 0:
+
+        for i in range(bboxes.shape[0]):
+            bbox = bboxes[i, :]
+            ax.add_patch(
+                plt.Rectangle((bbox[0], bbox[1]),
+                              bbox[2] - bbox[0],
+                              bbox[3] - bbox[1], fill=False,
+                              edgecolor='#17eb00', linewidth=3)
+            )
+
+    plt.axis('off')
+    plt.tight_layout()
+    plt.draw()
+    if visualization_folder is not None:
+        if not os.path.exists(visualization_folder):
+            os.makedirs(visualization_folder)
+        plt_name += ext
+        plt.savefig(os.path.join(visualization_folder,plt_name),bbox_inches='tight')
+        print('Saved {}'.format(os.path.join(visualization_folder, plt_name)))
+    else:
+        print('Visualizing {}!'.format(plt_name))
+        plt.show()
+    plt.clf()
+    plt.cla()
+
 
